b4379d113b7c593adc3f2be6490b5f41a4b6ef55
Anjaly1997 <anjaly1997soman@gmail.com>
2024-04-15 13:14:00 +0200

    Implementing no_std environment

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
+#![no_std]
+
 // Constants for the ring buffer
-const SIZE: usize = 256; 
-const MSGS: usize = 10; 
+const SIZE: usize = 256;
+const MSGS: usize = 10;
 
 #[derive(Debug)]
 enum QueueError {
     FullBuffer,
 }
 
-//message of fixed size
 #[derive(Debug)]
 struct Message([u8; SIZE]);
 
-// queueing port
 struct QueueingPort {
     buffer: [u8; SIZE * MSGS],
     write_index: usize,
@@ -20,17 +20,15 @@ struct QueueingPort {
 }
 
 impl QueueingPort {
-
     fn new() -> QueueingPort {
         QueueingPort {
-            buffer: [0; SIZE * MSGS], 
+            buffer: [0; SIZE * MSGS],
             write_index: 0,
             read_index: 0,
             message_count: 0,
         }
     }
 
-    // Adds a message to the queue
     fn enqueue(&mut self, message: Message) -> Result<(), QueueError> {
         if self.message_count >= MSGS {
             return Err(QueueError::FullBuffer);
@@ -46,7 +44,6 @@ impl QueueingPort {
         Ok(())
     }
 
-    // Removes a message from the queue
     fn dequeue(&mut self) -> Option<Message> {
         if self.message_count == 0 {
             return None;
@@ -56,27 +53,17 @@ impl QueueingPort {
         let mut msg_array = [0u8; SIZE];
         for i in 0..SIZE {
             msg_array[i] = self.buffer[start + i];
-            // Clear the buffer spot
             self.buffer[start + i] = 0;
         }
-        let message = Message(msg_array);
 
         self.read_index = (self.read_index + 1) % MSGS;
         self.message_count -= 1;
-        Some(message)
+        Some(Message(msg_array))
     }
 }
 
-fn main() {
-    let mut port = QueueingPort::new();
-    let msg = Message([1; SIZE]); 
-    match port.enqueue(msg) {
-        Ok(()) => println!("Message enqueued successfully"),
-        Err(QueueError::FullBuffer) => println!("Queue is full"),
-    }
-
-    match port.dequeue() {
-        Some(message) => println!("Message dequeued successfully: {:?}", message),
-        None => println!("Queue is empty"),
-    }
+// Define a panic handler
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
 }
